0e3db777a8db00fa2d51c01517b663c7fe463f9f
Mrittika Chakraborty <rhythm@Mrittikas-MacBook-Pro.local>
2025-11-21 15:59:29 -0500

    understanding the sliding window

--- Maxmimum-subarray.js
+++ Maxmimum-subarray.js
@@ -2,8 +2,11 @@
 You are given an integer array nums consisting of n elements, and an integer k.
 Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
 
-
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
 */ 
+
+nums = [1,12,-5,-6,50,3], k = 4
 
 var findMaxAverage = function(nums, k) {
     // Step 1: sum of first k elements
---
+++ finding-avg.js
@@ -0,0 +1,13 @@
+
+nums = [1,12,-5,-6,50,3], k = 4 
+
+ let findAvg = (nums, k) => {
+    let arraySum = 0;
+
+for (let i = 0; i < k; i++){
+    arraySum += nums[i];
+}
+return arraySum/k ;
+ }
+
+ console.log(findAvg(nums, k));
